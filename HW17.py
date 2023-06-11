ce404b526bbaad2deba2b1070a8d913247146c18
berezhkov1 <128647317+berezhkov1@users.noreply.github.com>
2023-06-11 14:03:42 +0300

    Update HW17.py

--- HW17.py
+++ HW17.py
@@ -1,13 +1,10 @@
 def binary_search(array, element, left, right):
-   # if left > right:  # если левая граница превысила правую,
-    #    return print("Введенный элемент не входит в рамки списка")  # значит элемент отсутствует
-    middle = (right + left) // 2  # находимо середину
-    if array[middle] < element <= array[middle+1]:  # если элемент в середине,
-        return middle  # возвращаем этот индекс
-    elif element < array[middle]:  # если элемент меньше элемента в середине
-        # рекурсивно ищем в левой половине
+    middle = (right + left) // 2 
+    if array[middle] < element <= array[middle+1]:
+        return middle
+    elif element < array[middle]: 
         return binary_search(array, element, left, middle - 1)
-    else:  # иначе в правой
+    else: 
         return binary_search(array, element, middle + 1, right)
 def sort(text):
     a = True
@@ -22,8 +19,8 @@ string = input("Введите числа через пробел:")
 x = int(input("Введите число:"))
 
 
-list_of_strings = string.split() # список строковых представлений чисел
-list_of_numbers = list(map(int, list_of_strings)) # список чисел
+list_of_strings = string.split() 
+list_of_numbers = list(map(int, list_of_strings)) 
 
 sort(list_of_numbers)
 print("Упорядоченный список введенных чисел:", list_of_numbers)
